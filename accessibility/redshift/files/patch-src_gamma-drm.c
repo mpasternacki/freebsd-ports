a01af145fe0d11ddecfe734a5243ee88b1b31f31
Maciej Pasternacki <maciej@3ofcoins.net>
2015-03-04 19:00:11 +0100

    Bump Redshift

---
+++ accessibility/redshift/files/patch-src_gamma-drm.c
@@ -0,0 +1,10 @@
+--- src/gamma-drm.c.orig	2015-02-18 20:53:47.461644000 +0100
++++ src/gamma-drm.c	2015-02-18 20:53:54.218707000 +0100
+@@ -21,7 +21,6 @@
+ #include <stdlib.h>
+ #include <stdint.h>
+ #include <string.h>
+-#include <alloca.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
